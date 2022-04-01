37788542c349d5fd0b9998c0d7be29cbd4c1db30
bernadto <bernardto0407@gmail.com>
2022-04-01 17:36:42 +0700

    Updates

---
+++ precache-manifest.ced2cc494a9ca158bfd927b4056d923e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "021ba785ca7d665e9c36c45f43bbfa73",
+    "url": "/myportfolio.github.io/index.html"
+  },
+  {
+    "revision": "b586dbd1a1f07d560030",
+    "url": "/myportfolio.github.io/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "5be3dd16e83933846868",
+    "url": "/myportfolio.github.io/static/css/main.739da350.chunk.css"
+  },
+  {
+    "revision": "b586dbd1a1f07d560030",
+    "url": "/myportfolio.github.io/static/js/2.ddac9b14.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/myportfolio.github.io/static/js/2.ddac9b14.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5be3dd16e83933846868",
+    "url": "/myportfolio.github.io/static/js/main.89f9e717.chunk.js"
+  },
+  {
+    "revision": "b1cb018adac39ab2bbe5",
+    "url": "/myportfolio.github.io/static/js/runtime-main.1f16b868.js"
+  }
+]);
